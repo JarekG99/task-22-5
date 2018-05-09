5363659a8c9e3b83a48622f777c6f6b8fe6517c0
Jarek Gajdulewicz <j.gajdulewicz@icloud.com>
2018-05-09 11:03:10 +0200

    First commit

---
+++ client/modules/Post/PostActions.js
@@ -0,0 +1,117 @@
+import callApi from '../../util/apiCaller';
+
+// Export Constants
+export const ADD_POST = 'ADD_POST';
+export const ADD_POSTS = 'ADD_POSTS';
+export const EDIT_POST = 'EDIT_POST';
+export const DELETE_POST = 'DELETE_POST';
+export const THUMB_UP = 'THUMB_UP';
+export const THUMB_DOWN = 'THUMB_DOWN';
+
+// Export Actions
+export function addPost(post) {
+  return {
+    type: ADD_POST,
+    post,
+  };
+}
+
+export function thumbUp(cuid, post) {
+  return {
+    type: THUMB_UP,
+    cuid,
+    post,
+  };
+}
+
+export function thumbDown(cuid, post) {
+  return {
+    type: THUMB_DOWN,
+    cuid,
+    post,
+  };
+}
+export function addPostRequest(post) {
+  return (dispatch) => {
+    return callApi('posts', 'post', {
+      post: {
+        name: post.name,
+        title: post.title,
+        content: post.content,
+        votes: post.votes,
+      },
+    }).then(res => dispatch(addPost(res.post)));
+  };
+}
+
+export function addPosts(posts) {
+  return {
+    type: ADD_POSTS,
+    posts,
+  };
+}
+
+export function fetchPosts() {
+  return (dispatch) => {
+    return callApi('posts').then(res => {
+      dispatch(addPosts(res.posts));
+    });
+  };
+}
+
+export function editThumbUpRequest(cuid, post) {
+  return (dispatch) => {
+    return callApi(`posts/${cuid}`, 'put', {
+      post: {
+        votes: post.votes,
+      },
+    }).then(() => { dispatch(thumbUp(cuid, post));
+   });
+ };
+}
+
+export function editThumbDownRequest(cuid, post) {
+  return (dispatch) => {
+    return callApi(`posts/${cuid}`, 'put', {
+      post: {
+        votes: post.votes,
+      },
+    }).then(() => { dispatch(thumbDown(cuid, post));
+   });
+  };
+}
+
+export function editPost(cuid, post) {
+  return {
+    type: EDIT_POST,
+      cuid,
+      post,
+  };
+}
+
+export function editPostRequest(cuid, post) {
+  return (dispatch) => {
+    return callApi(`posts/${cuid}`, 'put', {
+      post: {
+        name: post.name,
+        title: post.title,
+        content: post.content,
+        votes: post.votes,
+      },
+    }).then(() => { dispatch(editPost(cuid, post));
+    });
+  };
+}
+
+export function deletePost(cuid) {
+  return {
+    type: DELETE_POST,
+    cuid,
+  };
+}
+
+export function deletePostRequest(cuid) {
+  return (dispatch) => {
+    return callApi(`posts/${cuid}`, 'delete').then(() => dispatch(deletePost(cuid)));
+  };
+}
---
+++ client/modules/Post/components/PostList.js
@@ -0,0 +1,39 @@
+import React, { PropTypes } from 'react';
+
+// Import Components
+import PostListItem from './PostListItem/PostListItem';
+
+function PostList(props) {
+  return (
+    <div className="listView">
+      {
+        props.posts.map(post => (
+          <PostListItem
+            post={post}
+            key={post.cuid}
+            onDelete={() => props.handleDeletePost(post.cuid)}
+            onThumbUp={() => props.handleThumbUp(post.cuid, post)}
+            onThumbDown={() => props.handleThumbDown(post.cuid, post)}
+            votes={post.votes}
+          />
+        ))
+      }
+    </div>
+  );
+}
+
+PostList.propTypes = {
+  posts: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    slug: PropTypes.string.isRequired,
+    cuid: PropTypes.string.isRequired,
+    votes: PropTypes.number,
+  })).isRequired,
+  handleDeletePost: PropTypes.func.isRequired,
+  handleThumbUp: PropTypes.func.isRequired,
+  handleThumbDown: PropTypes.func.isRequired,
+};
+
+export default PostList;
